15994567695309cc1bf5a5d3d7758775c5e229ed
Iliyas <iliyas2008@gmail.com>
2022-01-23 15:23:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const handlePlus = ()=>{
+    if(counter<20){
+      setCounter(counter+1)
+    }else{
+      setCounter(0)
+    }
+  }
+  const handleMinus = ()=>{
+    if(counter>0){
+      setCounter(counter-1)
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Custom Counter</h1>
+        <h3>Counter works from 0 through 20 (above or below will reset to zero)</h3>
       </header>
+      <section className="App-body">
+        <button onClick={handlePlus} className="btn">+</button>
+          <h2>{counter}</h2>
+        <button onClick={handleMinus} className="btn">-</button>
+      </section>
     </div>
   );
 }
